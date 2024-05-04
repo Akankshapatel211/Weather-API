7d675898e4e90dcdb6c0f28a5d13424fe6c78500
Akankshapatel211 <akt1292002@gmail.com>
2024-05-04 16:04:20 +0530

    First commit

---
+++ src/main/java/com/dice/rapidapi/Config/JWT_Constant.java
@@ -0,0 +1,6 @@
+package com.dice.rapidapi.Config;
+
+public class JWT_Constant {
+    public static final String SECRET_KEY = "agsdfhasgdfhkjaAPsfdjasdfhjagsdfhjgvasdhjfghajsdgfhf";
+    public static final String JWT_HEADER = "Authorization";
+}
